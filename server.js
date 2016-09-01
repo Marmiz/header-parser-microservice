a3dc2725b1b15d5b9e8cf9f562d62e7f8c20e108
claudio restifo <claudio.restifo@gmail.com>
2016-09-01 05:11:39 +0000

    added server functionality

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 var express = require ('express');
 var app = express();
 
+// set port
+app.set('port', (process.env.PORT || 8080));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  // create an object and populate it with the desired info
+  var info = {
+    'ip' : req.headers["x-forwarded-for"],
+    'language' : req.headers["accept-language"].split(',')[0],
+    'software' : req.headers["user-agent"].split('(')[1].split(')')[0],
+  };
+  
+  // print it on screen.
+  res.json(info);
+  
+  // to see the complete req.header object
+  // console.log(req.headers);
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.listen(app.get('port'), function () {
+  console.log(`Server listening on port ${app.get('port')}`);
 });
